92000d4bae04c87ab8bf1880c6b70f26950e24b4
Syarifudin <suicideudin@gmail.com>
2025-04-29 17:27:38 +0700

    update migrations

--- database/migrations/2024_10_19_222713_create_report_details_table.php
+++ database/migrations/2024_10_19_222713_create_report_details_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->foreignId('report_id')->constrained('reports'); // ID laporan (foreign key)
             $table->decimal('total_sales', 10, 2); // Total penjualan untuk laporan ini
             $table->decimal('total_expenses', 10, 2); // Total pengeluaran untuk laporan ini
-            $table->timestamp('period_start'); // Tanggal mulai periode laporan
-            $table->timestamp('period_end'); // Tanggal akhir periode laporan
+            $table->timestamp('period_start')->nullable(); // Tanggal mulai periode laporan
+            $table->timestamp('period_end')->nullable(); // Tanggal akhir periode laporan
             $table->timestamps();
         });
     }
